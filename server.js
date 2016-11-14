72ae40832d8b3b1daac97c1668e93ada20a86d40
Marvin Flores <elmarvinflores@gmail.com>
2016-11-14 15:13:21 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require("method-override");
+var app = express();
+var aux = "";
+// Connection to DB
+mongoose.connect('mongodb://localhost:27017/clients', function(err, res) {
+ if(err){
+   console.log('No Connected to Database');
+   throw err;
+ } else {
+   console.log('Connected to Database');
+ }
+
+});
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and Controllers
+var models     = require('./models/client')(app, mongoose);
+var ClientCtrl = require('./controllers/clients');
+
+var router = express.Router();
+
+// Index - Route
+router.get('/', function(req, res) {
+   res.send("Hola Mundo - www.programacion.com.py -"+aux);
+});
+
+app.use(router);
+
+// API routes
+var api = express.Router();
+
+api.route('/clients')
+  .get(ClientCtrl.findAll)
+  .post(ClientCtrl.add);
+
+api.route('/clients/:id')
+  .get(ClientCtrl.findById)
+  .put(ClientCtrl.update)
+  .delete(ClientCtrl.delete);
+
+app.use('/api', api);
+
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
