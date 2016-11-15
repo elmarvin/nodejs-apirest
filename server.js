e41de6b6c692b924c72523455c21fc393475dc34
Marvin Flores <elmarvinflores@gmail.com>
2016-11-14 22:51:38 -0500

    Server Listen Configurado

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.use('/api', api);
 
 
 // Start server
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
-});
+//app.listen(3000, function() {
+//  console.log("Node server running on http://localhost:3000");
+//});
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
